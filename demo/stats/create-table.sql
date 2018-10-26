cd88f93e028eb6834901858f5bb40b65be04e469
Dave Pitts <dgapitts@gmail.com>
2018-10-26 14:47:28 +0000

    add create-table.sql for stats demo

---
+++ demo/stats/create-table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE zip_codes (
+  postal_code       VARCHAR(20),
+  place_name        VARCHAR(180),
+  state_name        VARCHAR(100),
+  province_name     VARCHAR(100),
+  community_name    VARCHAR(100),
+  latitude          REAL,
+  longitude         REAL
+);
