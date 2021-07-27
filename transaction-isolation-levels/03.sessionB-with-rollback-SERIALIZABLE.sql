57f8667075bdca74f1e45040bb3e5474e7742fdd
Dave Pitts <dgapitts@gmail.com>
2021-07-27 23:07:55 +0200

    sparallel transactions and then rollback SERIALIZABLE

--- transaction-isolation-levels/03.sessionB-with-rollback-SERIALIZABLE.sql
+++ transaction-isolation-levels/03.sessionB-with-rollback-SERIALIZABLE.sql
@@ -1,11 +1,8 @@
--- based off https://www.cockroachlabs.com/docs/stable/demo-serializable.html
-
 \timing on
 BEGIN;
 
 set transaction ISOLATION LEVEL SERIALIZABLE;
 show transaction ISOLATION LEVEL;
-
 
 
 SELECT day, count(*) AS doctors_on_call FROM schedules
@@ -17,19 +14,20 @@ SELECT day, count(*) AS doctors_on_call FROM schedules
 SELECT count(*) FROM schedules
   WHERE on_call = true
   AND day = '2018-10-05'
-  AND doctor_id != 1;
+  AND doctor_id != 2;
 
 -- pause for parallel session2 to start 
 select now(), pg_sleep(10);
 
 UPDATE schedules SET on_call = false
   WHERE day = '2018-10-05'
-  AND doctor_id = 1;
+  AND doctor_id = 2;
 
+select now(), pg_sleep(10);
 
 SELECT day, count(*) AS doctors_on_call FROM schedules
   WHERE on_call = true
   GROUP BY day
   ORDER BY day;
 
-rollback;
+rollback;  
